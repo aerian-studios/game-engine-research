2342c872f42835e469a7d115f32b8b2116a1c48a
S-unya <sunya.dickman@aerian.com>
2018-09-20 17:33:43 +0100

    moveable character beginings of obstacle

--- godot/helloworld/Player.gd
+++ godot/helloworld/Player.gd
@@ -1,32 +1,46 @@
 extends Area2D
 
-# class member variables go here, for example:
-export (int) var speed  # How fast the player will move (pixels/sec).
-var screensize  # Size of the game window.
+signal hit
+
+export (int) var SPEED
+var velocity = Vector2()
+var screensize
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	#hide()
 	screensize = get_viewport_rect().size
-
-
-func _process(delta):
-	# Called every frame
-    var velocity = Vector2() # The player's movement vector.
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed
-        $AnimatedSprite.play()
-    else:
-        $AnimatedSprite.stop()
 	
-	# position += velocity * delta
-	# position.x = clamp(position.x, 0, screensize.x)
-	# position.y = clamp(position.y, 0, screensize.y)
+func start(pos):
+	position = pos
+	show()
+	$Collision.disabled = false
+			
+func _process(delta):
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * SPEED
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+
+func _on_Player_body_entered( body ):
+	$Collision.disabled = true
+	hide()
+	emit_signal("hit")
